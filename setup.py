7fdde17940a264a2a836d48fe3923fce1531d8ae
rahil <rahiljc15@gmail.com>
2025-10-09 00:41:25 +0530

    Added Folder structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MediBot",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here, e.g. 'requests', 'flask'
+    ],
+    author="Mohammadrahil Nasardi",
+    description="A medical bot project.",
+    python_requires=">=3.6",
+)
